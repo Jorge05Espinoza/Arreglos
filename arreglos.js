9463795173f509acc73c68e89483b7d1605b640f
Pedro <benito05camela@gmail.com>
2023-10-03 14:19:16 -0700

    arreglos

---
+++ arreglos.js
@@ -0,0 +1,33 @@
+//let arreglo = [
+   // { Numero: 65 },
+   // { Numero: 2 },
+   // { Numero: 13 },
+   // { Numero: 3 },
+  //  { Numero: 25 }
+ // ];
+  
+ // let mayor = arreglo.filter(numero => numero.Numero > 5);
+  
+ // mayor.forEach(numero => {
+ //   console.log(numero);
+ // });
+
+ 
+
+// let arregloFrutas = ["naranja", "platano", "manzana", "fresa", "kiwi"];
+
+// let frutas = arregloFrutas.slice(2, 4);
+ //console.log(frutas);
+
+// let arregloMap = [1, 2, 3, 4, 5];
+// arregloMap.map(function(numero, indice) {
+//   console.log(`${indice} : ${numero+10}`);
+ //});
+
+// let arregloLetras=["H","I","J"];
+// let arregloLetras2=["K","L","M"];
+// let arregloLetras3=arregloLetras.concat(arregloLetras2);
+// console.log(arregloLetras3);
+
+let  arregloPaises=["Mexico","USA","Francia"];
+console.log(arregloPaises.includes("Francia"))
